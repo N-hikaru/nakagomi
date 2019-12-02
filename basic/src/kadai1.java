af5ad1eb89bae4c4493051f5c84588d2a61c8a02
N-hikaru <nh930928@icloud.com>
2019-12-02 16:33:37 +0900

    Javaの基礎_課題提出

---
+++ basic/src/kadai1.java
@@ -0,0 +1,9 @@
+
+public class kadai1 {
+
+	public static void main(String[] args) {
+		System.out.println("初めての課題");
+		System.out.print("Javaの攻略");
+	}
+
+}
---
+++ basic/src/kadai2.java
@@ -0,0 +1,9 @@
+
+public class kadai2 {
+
+	public static void main(String[] args) {
+		System.out.println("hogehoge");
+		System.out.println("hogehoge2");
+	}
+
+}
