aea6f0c100f3858704cf52525e8b783c50c830ec
N-hikaru <nh930928@icloud.com>
2019-12-05 07:27:03 +0900

    四則演算_課題提出

---
+++ 四則演算/src/kadai2.java
@@ -0,0 +1,19 @@
+
+public class kadai2 {
+
+	public static void main(String[] args) {
+
+		int intNum1 = 10;
+		int intNum2 = 12;
+		int intNum3 = 13;
+			System.out.println("合計:\b"+(intNum1+intNum2+intNum3));
+
+		int intNum4 = intNum1+intNum2+intNum3;
+			System.out.println("平均(3で割った時):\b"+intNum4/3);
+
+		double doubleNum = (double)intNum4/3.0;
+			System.out.println("平均(3.0で割った時):\b"+doubleNum);
+
+	}
+
+}
