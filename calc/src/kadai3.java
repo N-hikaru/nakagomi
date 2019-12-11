efc066c042ce5554a9905705420442c2b1829b39
N-hikaru <nh930928@icloud.com>
2019-12-11 16:25:56 +0900

    四則演算_再提出

--- calc/src/kadai3.java
+++ calc/src/kadai3.java
@@ -9,29 +9,30 @@ public class kadai3 {
 		int intNum1 = scan.nextInt();
 		int intNum2 = scan.nextInt();
 		int intNum3 = scan.nextInt();
-		//出力
-		System.out.println(intNum1);
-		System.out.println(intNum2);
-		System.out.println(intNum3);
+		scan.close();
+			//出力
+			System.out.println(intNum1);
+			System.out.println(intNum2);
+			System.out.println(intNum3);
 
 		//変数の初期化(合計値)
 		int intNum4 = intNum1 + intNum2 + intNum3;
-		//合計値を出力
-		System.out.println("合計:\b"+intNum4);
+			//合計値を出力
+			System.out.println("合計: "+intNum4);
 
 		//平均(3で割った時)
 		//変数の初期化(平均(3で割った時))
         int intNum5 = intNum4/3;
-        //出力
-        System.out.println("平均(3割った時)"+intNum5);
+        	//出力
+        	System.out.println("平均(3割った時):"+intNum5);
 
         //平均(3.0で割った時)
         //型変換
         double doubleNum1 = intNum4;
         //変数の初期化(平均(3.0で割った時))
         double doubleNum2 = doubleNum1/3.0;
-        //出力
-        System.out.println("平均(3.0で割った時)"+doubleNum2);
+        	//出力
+        	System.out.println("平均(3.0で割った時):"+doubleNum2);
 
 	}
 
