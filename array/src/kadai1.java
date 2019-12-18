de90bec034f69ae141e35feced4cd44f021f69f6
N-hikaru <nh930928@icloud.com>
2019-12-18 20:52:46 +0900

    配列_再提出

--- array/src/kadai1.java
+++ array/src/kadai1.java
@@ -5,21 +5,22 @@ public class kadai1 {
 		System.out.print("配列: [");
 		//int型の配列に格納
 		int[]  array = {4,9,19,5,8,21,42,64,73,18,2};
-		int i1;
-		int i2 = 0;
+
+		//array[i]
+		int i;
 		//数字を出力させる処理
-		for(i1 = 0;i1 < array.length;i1++) {
-			System.out.print(array[i1]);
+		for(i = 0 ; i < array.length ; i++) {
+			System.out.print(array[i]);
 			//「,」を出力させる処理
-			if(i2 < (array.length-1)){
-				i2++;
+			if(i < (array.length-1)){
+				i++;
 				System.out.print(",");
-			}
+ 			}
 		}
 		System.out.println("]");
+
         //合計を出力させる処理
 		System.out.print("合計: ");
-
 		int total = 0;
 		for(int intNum : array) {
 			total += intNum;
--- array/src/kadai3.java
+++ array/src/kadai3.java
@@ -6,45 +6,42 @@ public class kadai3 {
 
 		System.out.print("[");
 		//要素数5の配列
+		//int[] nums = new int[i]
 		int[] nums = new int[6];
 
 		//ランダムに0～9の数字を6回出力させる
 		Random random = new Random();
-		int i1 = 1;
-		for(int i2 = 0; i2 < 6 ; i2++) {
+		int k = 1;
+		for(int i = 0; i < 6 ; i++) {
 			//int Ran = random.nextInt(9);
-			nums[i2]= random.nextInt(9);
-				System.out.print(nums[i2]);
+			nums[i]= random.nextInt(9);
+				System.out.print(nums[i]);
 			//「,」を5回出力させる
-			if(i1 < 6) {
-				i1++;
+			if(k < (nums.length)) {
+				k++;
 				System.out.print(",");
 			}
 		}
 		System.out.println("]");
 
-		System.out.print("最大値: ");
-		//最大値を求める
-		//nums[0]が暫定的に最大値とする
+		//最大値・最小値を求める
+		//nums[0]が暫定的に最大値・最小値とする
 		int max = nums[0];
-		for (int i3 = 1 ; i3 < nums.length ; i3++) {
-		    if (max < nums[i3]  ) {
-		    	//比べた結果暫定の最大値より大きい場合には変数maxの数値を置きかえる
-		        max = nums[i3];
-		    }
-		}
-		System.out.println(max);
-
-		System.out.print("最小値: ");
-		//最小値を求める
 		int min = nums[0];
-		for(int i3 = 1 ; i3 < nums.length ; i3++) {
-			if(nums[i3] < min) {
-				min = nums[i3];
-			}
+		for (int s = 1 ; s < nums.length ; s++) {
+		    if (max < nums[s]  ) {
+		    	//比べた結果暫定の最大値より大きい場合には変数maxの数値を置きかえる
+		        max = nums[s];
+		    }
+		    if(nums[s] < min) {
+		    	//比べた結果暫定の最小値より小さい場合には変数minの数値を置き換える
+				min = nums[s];
+				}
 		}
-		System.out.println(min);
+		System.out.println("最大値: " + max);
+		System.out.println("最小値: " + min);
 	}
 }
+
 
 
